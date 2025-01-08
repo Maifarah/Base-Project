c2ef03505e72125d51db0c7794ac296d7d6de335
Mai Farah <maifarah423@gmail.com>
2025-01-08 15:14:51 +0200

    doneUI for  Edite note view

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -18,7 +18,7 @@ class CustomTextField extends StatelessWidget {
       decoration: InputDecoration(
         hintText: hint,
         hintStyle: const TextStyle(
-          color: kPrimaryColor,
+          color: Colors.grey,
         ),
         border: buildBorder(),
         focusedBorder: buildBorder(kPrimaryColor),
--- lib/widgets/edite_note_view_body.dart
+++ lib/widgets/edite_note_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:base_project/widgets/custom_appbar.dart';
+import 'package:base_project/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class EditeNoteViewBody extends StatelessWidget {
@@ -6,7 +7,7 @@ class EditeNoteViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
@@ -16,6 +17,17 @@ class EditeNoteViewBody extends StatelessWidget {
           CustomAppBar(
             title: 'Edite Notes',
             icon: Icons.check,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(hint: 'Title'),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 5,
           ),
         ],
       ),
